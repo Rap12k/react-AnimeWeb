121a603adb620d87e3fed45fc7d77fad3bd81563
Rap12k <robpitter101@gmail.com>
2019-08-03 21:18:39 -0400

    testing

--- index.js
+++ index.js
@@ -6,13 +6,13 @@ import AnimeRow from './AnimeRow.js';
 class App extends Component {
   constructor() {
     this.state = {
-      rows: [
-        <p key="1">Test</p>,
-        <p key="2">Test</p>,
-      ]
+      searchString: '',
+      picked: 0,
+      value: 0,
+      disabled: false,
     };
 
-    const animes = [
+    /*const animes = [
       {id: 0, title: "Anime Title", synopsis: "As the Fourth Holy Grail War rages on with no clear victor in sight, the remaining Servants and their Masters are called upon by Church supervisor Risei Kotomine, in order to band together and confron...", image_url: "https://1.bp.blogspot.com/-lUDcZyriPL4/XQVfQnw3ReI/AAAAAAAAoZw/4qk-I3SVCaITsn7vbxBp0SVKV0l4GIgkACLcBGAs/w180/ta_31-the-two-gets-by-tomorrow-too-more-or-less.jpg"},
       {id: 1, title: "Anime Title", synopsis: "As the Fourth Holy Grail War rages on with no clear victor in sight, the remaining Servants and their Masters are called upon by Church supervisor Risei Kotomine, in order to band together and confron...", image_url: "https://1.bp.blogspot.com/-lUDcZyriPL4/XQVfQnw3ReI/AAAAAAAAoZw/4qk-I3SVCaITsn7vbxBp0SVKV0l4GIgkACLcBGAs/w180/ta_31-the-two-gets-by-tomorrow-too-more-or-less.jpg"}
     ]
@@ -24,7 +24,67 @@ class App extends Component {
       animeRows.push(animeRow)
     })
 
-    this.state = {rows: animeRows}
+    this.state = {rows: animeRows}*/
+    this.performSearch()
+  }
+
+  performSearch() {
+    console.log("starting search")
+    const query = this._urlQuery();
+    this._executeQuery(query);
+  }
+  _urlQuery = () => {
+    const params = {
+      q: this.state.searchString,
+      limit: 50,
+      type: this.state.picked
+    };
+    var key;
+    key = params[key];
+    var options = ["anime", "manga"]
+    var selectedOption = options[this.state.value]
+    
+    const querystring = Object.keys(params)
+      .map(key => key + '=' + encodeURIComponent(params[key]))
+      .join('%');
+    switch (this.state.value) {
+      case 0:
+      case 1:
+        return `https://api.jikan.moe/v3/search/${selectedOption}?` + querystring;
+        break;
+    }
+  }
+  _executeQuery = (query) => {
+    this.setState(
+      {
+        disabled: true,
+      });
+      switch (this.state.value) {
+        case 0:
+          fetch(query)
+            .then(response => response.json())
+            .then(responseJson => {
+              this.setState({
+                disabled: false,
+                });
+              const results = responseJson.results
+            })
+            .catch(error => {
+              console.error(error);
+            });
+        case 1:
+          fetch(query)
+            .then(response => response.json())
+            .then(responseJson => {
+              this.setState({
+                disabled: false,
+                });
+              const results = responseJson.results
+            })
+            .catch(error => {
+              console.error(error);
+            });
+      }
   }
 
   render() {
